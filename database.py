001ad3681e2bf05160e1acd6c1e9d8e9f4bf624f
Maksim <knyazev.maksim2852@gmail.com>
2024-10-01 09:35:34 +0400

    add database

---
+++ database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+import os
+
+
+
+DATABASE_NAME: str = os.getenv('DATABASE_NAME')
+
+engine = create_async_engine(DATABASE_NAME)
+async_session_maker = async_sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
