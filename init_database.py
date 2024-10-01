4094a24b44252ecd534f8f538257646be5c9cdbc
Maksim <knyazev.maksim2852@gmail.com>
2024-10-01 09:35:46 +0400

    add init db

---
+++ init_database.py
@@ -0,0 +1,8 @@
+from bot.database import Base, engine
+from bot.src.models.user import User
+from bot.src.models.establishment import Establishment
+from bot.src.models.establishment_type import EstablishmentType
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
