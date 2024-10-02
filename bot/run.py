3b35e5ab0bb2583080fe2522a693e7091205e362
Maksim <knyazev.maksim2852@gmail.com>
2024-10-02 17:48:16 +0400

    update main file

---
+++ bot/run.py
@@ -0,0 +1,47 @@
+from json import JSONDecodeError
+from aiogram import Bot, Dispatcher, types
+from aiogram.client.default import DefaultBotProperties
+from logger import logger
+from bot.src.handlers.user_handlers import user_router
+import os
+from fastapi import Request, Response
+from bot.src.midleware import DbMiddleware
+from database import async_session_maker
+
+
+TOKEN_BOT: str = os.getenv('TOKEN_BOT')
+web_hook: str = f"/{TOKEN_BOT}"
+WEBHOOK_URL: str = os.getenv('WEBHOOK_URL')
+bot: Bot = Bot(TOKEN_BOT, default=DefaultBotProperties(parse_mode="HTML"))
+dp: Dispatcher = Dispatcher()
+
+
+async def set_webhook():
+    webhook_url: str = f'{WEBHOOK_URL}{web_hook}'
+    await bot.set_webhook(webhook_url, allowed_updates=["callback_query", "message"])
+
+
+async def on_startup():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await set_webhook()
+    
+
+async def handle_web_hook(request: Request):
+    url: str = str(request.url)
+    index: str = url.rfind("/")
+    token: str = url[index + 1 :]
+    if token == TOKEN_BOT:
+        try:
+            request_data = await request.json()
+            update = types.Update(**request_data)
+            await dp.feed_webhook_update(bot, update)
+            return Response()
+        except JSONDecodeError:
+            logger.error("Ошибка декодирования Json")
+    else:
+        return Response(status_code=403)
+
+
+dp.include_router(user_router)
+dp.startup.register(on_startup)
+dp.message.middleware.register(DbMiddleware(async_session_maker))
