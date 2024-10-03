65f72aef7cf3a32642601e5f947d6d7014bbcefa
Maksim <knyazev.maksim2852@gmail.com>
2024-10-03 19:33:46 +0400

    update filter

--- bot/src/handlers/admin_filter.py
+++ bot/src/handlers/admin_filter.py
@@ -3,13 +3,13 @@ from aiogram.types import Message
 import os
 
 
-ADMINS: list[int] = os.getenv('ADMINS_ID')
+ADMINS: list[int] = os.getenv('ADMINS_ID').split(',')
 
 
 class AdminProtect(Filter):
 
     def __init__(self):
-        self.admins: list[int] = ADMINS
+        self.admins: list[int] = list(map(int, ADMINS))
 
     async def __call__(self, message: Message):
         return message.from_user.id in self.admins
