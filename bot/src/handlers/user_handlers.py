a8fc6fd07df908fe1bfee2b791ab33096d56a578
Maksim <knyazev.maksim2852@gmail.com>
2024-10-05 20:13:02 +0400

    added the output of all establishments

--- bot/src/handlers/user_handlers.py
+++ bot/src/handlers/user_handlers.py
@@ -1,38 +1,91 @@
+from typing import Any
 from aiogram import Bot, F, Router
 from aiogram.filters import Command, CommandStart, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state
-from aiogram.types import CallbackQuery, Message, ReplyKeyboardRemove, WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import CallbackQuery, Message
 from geopy.geocoders import Yandex
 import os
+from bot.src.models.establishment import Establishment
 from bot.src.repository.user_repository import UserRepository
-from bot.src.repository.sqlalchemy_repositroy import SQLAlchemyRepository
 from bot.src.models.user import User
 from sqlalchemy.ext.asyncio import AsyncSession
+from bot.src.schemas.establishment import EstablishmentSchemaOut
 from bot.src.states.location import LocationState
-from bot.src.keyboards.keyboards import keyboard
+from bot.src.keyboards.inline_keyboards import get_pagination_keyboard
+from bot.src.repository.establishments_repository import EstablishmentRepository
+
 
 
 user_router: Router = Router()
 api_key: str = os.getenv('API_KEY')
 geolocator: Yandex = Yandex(api_key=api_key)
+token = os.getenv('TOKEN_BOT')
+
+
 
 
 @user_router.message(CommandStart(), StateFilter(default_state))
 async def cmd_start(message: Message, session: AsyncSession):
     user: User = await UserRepository.find_one_or_none(session=session, user_id=message.from_user.id)
     if not user:
-        await UserRepository.add(session=session)
-    await message.answer('Выбери', reply_markup=keyboard)
+        await UserRepository.add(session=session, user_id=message.from_user.id)
+
+    current_page: int = 1
+    offset: int = (current_page - 1) * 10
+
+    res, total_count = await EstablishmentRepository.find_all_limit_offset(session=session, limit=10, offset=offset)
+    establishments: list[EstablishmentSchemaOut] = [EstablishmentSchemaOut.model_validate(est) for est in res]
+
+    total_pages: int = (total_count - 1) // 10 + 1
     
 
-
-@user_router.message(StateFilter(default_state), F.text == 'Рестораны')
-async def get_restaurants(message: Message):
     await message.answer(
-        'вот так',
-        reply_markup=ReplyKeyboardRemove()
+        f'Рестораны:\n',
+        reply_markup=get_pagination_keyboard(establishments=establishments, current_page=current_page, total_pages=total_pages)
     )
+
+
+@user_router.callback_query(StateFilter(default_state), F.data.startswith('page_'))
+async def handle_pagination(callback: CallbackQuery, session: AsyncSession):
+    await callback.answer()
+    page: int = int(callback.data.split("_")[1])
+    offset: int = (page - 1) * 10
+    res, total_count = await EstablishmentRepository.find_all_limit_offset(session=session, limit=10, offset=offset)
+    establishments: list[EstablishmentSchemaOut] = [EstablishmentSchemaOut.model_validate(est) for est in res]
+
+    total_pages: int = (total_count - 1) // 10 + 1
+    if page > 0 and page <= total_pages:
+        await callback.message.edit_text(
+            'Все рестораны: \n',
+            reply_markup=get_pagination_keyboard(establishments, page, total_pages)
+        )
+        
+
+
+@user_router.callback_query(StateFilter(default_state), F.data.startswith('total_pages'))
+async def total_pages(callback: CallbackQuery):
+    res: str = callback.data.split('_')[2]
+    await callback.answer(f'Страницы: {res}')
+
+
+@user_router.callback_query(StateFilter(default_state), F.data.startswith('establishment_'))
+async def get_establishment(callback: CallbackQuery, session: AsyncSession):
+    res: str = callback.data.split('_')[1]
+    establisment: Establishment = await EstablishmentRepository.find_one_or_none(session=session, id=int(res))
+    establisment_out: EstablishmentSchemaOut = EstablishmentSchemaOut.model_validate(establisment)
+    await callback.message.edit_text(text='Информация о ресторане:')
+    await callback.message.answer_photo(
+        photo=establisment.photo_url,
+        caption=
+        f'<b>{establisment_out.title}</b>\n\n'
+        f'<b>{establisment_out.description}</b>\n\n'
+        f'<b>{establisment_out.address}</b>'
+    )
+
+
+
+
     
     
 
@@ -46,7 +99,8 @@ async def cmd_location(message: Message, state: FSMContext):
 
 
 @user_router.message(StateFilter(LocationState.location), F.location)
-async def get_location(message: Message, state: FSMContext):
+async def get_location(message: Message, state: FSMContext, session: AsyncSession):
+    
     pass
     
 
