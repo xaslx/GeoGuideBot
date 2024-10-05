b072352127897ab1d850e7655284eef19706b5f8
Maksim <knyazev.maksim2852@gmail.com>
2024-10-05 20:07:37 +0400

    update init db

--- init_database.py
+++ init_database.py
@@ -1,7 +1,6 @@
 from database import Base, engine
 from bot.src.models.user import User
 from bot.src.models.establishment import Establishment
-from bot.src.models.establishment_type import EstablishmentType
 
 async def init_db():
     async with engine.begin() as conn:
