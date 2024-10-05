df607860e457315cfdfcd66a6cb557b32db700b5
Maksim <knyazev.maksim2852@gmail.com>
2024-10-05 20:10:22 +0400

    update repository

--- bot/src/repository/establishment_types.py
+++
@@ -1,8 +0,0 @@
-from bot.src.repository.sqlalchemy_repositroy import SQLAlchemyRepository
-from bot.src.models.establishment_type import EstablishmentType
-
-
-
-class EstablishmentTypeRepository(SQLAlchemyRepository):
-
-    model: EstablishmentType = EstablishmentType
--- bot/src/repository/establishments_repository.py
+++ bot/src/repository/establishments_repository.py
@@ -1,8 +1,22 @@
 from bot.src.repository.sqlalchemy_repositroy import SQLAlchemyRepository
 from bot.src.models.establishment import Establishment
-
-
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import func, select
 
 class EstablishmentRepository(SQLAlchemyRepository):
 
     model: Establishment = Establishment
+
+
+    @classmethod
+    async def find_all_limit_offset(cls, session: AsyncSession, limit: int, offset: int = 0):
+        query = select(cls.model).limit(limit).offset(offset).order_by(cls.model.title)
+        res = await session.execute(query)
+
+        stmt_count = (
+            select(func.count()).select_from(cls.model)
+        )
+        res_count = await session.execute(stmt_count)
+        total_count: int = res_count.scalar()
+        return res.scalars().all(), total_count
+        
