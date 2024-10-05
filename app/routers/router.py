a47a9303185732b14ad2c4d657a41506e419c222
Maksim <knyazev.maksim2852@gmail.com>
2024-10-05 09:16:49 +0400

    update router

--- app/routers/router.py
+++ app/routers/router.py
@@ -1,12 +1,9 @@
-from fastapi import APIRouter, Request, Response
+from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import RedirectResponse
 from geopy.geocoders import Yandex
 import os
-from bot.src.schemas.establishments import Establishment
 from bot.src.utils.token import valid_token
 import os
-import asyncio
 
 
 
@@ -29,6 +26,7 @@ async def get_map(
     start_address: str,
     end_address: str
 ):
+ 
     return template.TemplateResponse(
         request=request,
         name='index.html',
@@ -43,25 +41,3 @@ async def get_map(
 
 
 
-
-
-
-@router.get('/add_establishment')
-async def add_establishment(
-    request: Request,
-    token: str
-):
-    user_id: int | None  = valid_token(token=token)
-
-    if not user_id or user_id['user_id'] not in ADMINS:
-        return template.TemplateResponse(
-            request=request,
-            name='403.html',
-            context={},
-            status_code=403
-        )
-    return template.TemplateResponse(
-        request=request,
-        name='add_establishment.html',
-        context={}
-    )
